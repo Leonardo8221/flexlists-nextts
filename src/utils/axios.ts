bb7348312a1657ff4c91ce70e18d4607ad0a45d8
Leonardo8221 <r.leonardo0802@gmail.com>
2023-05-12 10:19:27 +0700

    call to external flexist api

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // ----------------------------------------------------------------------
 
-const axiosInstance = axios.create({baseURL:process.env.REACT_APP_API_URL});
+const axiosInstance = axios.create({baseURL:process.env.NEXT_PUBLIC_FLEXLIST_API_URL});
 
 axiosInstance.interceptors.request.use(function (config) {
   return config;
