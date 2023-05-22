cc31d2f15bd533808db3598c769e6c2c3f32a043
Leonardo8221 <r.leonardo0802@gmail.com>
2023-05-22 10:58:11 +0700

    add dummy for frontend guy can work

--- src/redux/actions/listContentActions.ts
+++ src/redux/actions/listContentActions.ts
@@ -11,11 +11,12 @@ any
 > => {
   return async (dispatch: Dispatch<any>) => {
     try {
-      const response = await listContentService.getListContents(1);
-      if(response && response.code == 0)
-      {
-        dispatch(setRows(response.result));
-      } 
+      // const response = await listContentService.getListContents(1);
+      // if(response && response.code == 0)
+      // {
+      //   dispatch(setRows(response.result));
+      // } 
+      dispatch(setRows(getListContents()))
     } catch (error) {
      console.log('errr')
     }
@@ -27,3 +28,397 @@ export const setRows = (rows: any) => ({
   type: 'SET_ROWS',
   payload: rows
 });
+const getListContents = () : any[] =>{
+   return [
+    {
+      "1": 1,
+      "2": 'Task1',
+      "3": 'test',
+      "4": 'John',
+      "5": 'Very important',
+      "6": 'Done',
+      "7": '03/20/2023 08:30:00',
+      "8": '12',
+      "9": {
+        lat: 49.84120,
+        lng: 24.02975
+      },
+      "10": false
+    },
+    {
+      "1": 2,
+      "2": 'Task2',
+      "3": 'Lorem ipsum dolor sit amet consectetur. Tempus aliquam tortor ullamcorper vestibulum sit. Ac velit lectus quis non tortor scelerisque in velit.',
+      "4": 'Smith',
+      "5": 'Standard',
+      "6": 'In progress',
+      "7": '03/22/2023 10:45:00',
+      "8": '99',
+      "9": {
+        lat: 49.62120,
+        lng: 24.12975
+      },
+      "10": false
+    },
+    {
+      "1": 3,
+      "2": 'Task3',
+      "3": 'test',
+      "4": 'Smith',
+      "5": 'Standard',
+      "6": 'In progress',
+      "7": '03/24/2023 09:15:00',
+      "8": '99',
+      "9": {
+        lat: 49.53120,
+        lng: 24.32975
+      },
+      "10": false
+    },
+    {
+      "1": 4,
+      "2": 'Task4',
+      "3": 'test',
+      "4": 'John',
+      "5": 'Very important',
+      "6": 'Done',
+      "7": '03/24/2023 09:30:00',
+      "8": '12',
+      "9": {
+        lat: 49.44120,
+        lng: 24.43975
+      },
+      "10": false
+    },
+    {
+      "1": 5,
+      "2": 'Task5',
+      "3": 'test',
+      "4": 'Smith',
+      "5": 'Standard',
+      "6": 'In progress',
+      "7": '03/24/2023 09:00:00',
+      "8": '99',
+      "9": {
+        lat: 49.54120,
+        lng: 24.34975
+      },
+      "10": false
+    },
+    {
+      "1": 6,
+      "2": 'Task6',
+      "3": 'test',
+      "4": 'John',
+      "5": 'Very important',
+      "6": 'Testing',
+      "7": '03/24/2023 09:45:00',
+      "8": '12',
+      "9": {
+        lat: 49.65120,
+        lng: 24.21975
+      },
+      "10": false
+    },
+    {
+      "1": 7,
+      "2": 'Task7',
+      "3": 'test',
+      "4": 'John',
+      "5": 'Very important',
+      "6": 'Done',
+      "7": '03/27/2023 08:30:00',
+      "8": '12',
+      "9": {
+        lat: 49.75120,
+        lng: 24.32975
+      },
+      "10": false
+    },
+    {
+      "1": 8,
+      "2": 'Task8',
+      "3": 'Lorem ipsum dolor sit amet consectetur. Tempus aliquam tortor ullamcorper vestibulum sit. Ac velit lectus quis non tortor scelerisque in velit.',
+      "4": 'Smith',
+      "5": 'Standard',
+      "6": 'In progress',
+      "7": '03/29/2023 10:45:00',
+      "8": '99',
+      "9": {
+        lat: 49.95120,
+        lng: 24.13975
+      },
+      "10": false
+    },
+    {
+      "1": 9,
+      "2": 'Task9',
+      "3": 'test',
+      "4": 'Smith',
+      "5": 'Standard',
+      "6": 'In progress',
+      "7": '03/29/2023 09:15:00',
+      "8": '99',
+      "9": {
+        lat: 49.15120,
+        lng: 24.94975
+      },
+      "10": false
+    },
+    {
+      "1": 10,
+      "2": 'Task10',
+      "3": 'test',
+      "4": 'John',
+      "5": 'Very important',
+      "6": 'Done',
+      "7": '03/29/2023 09:30:00',
+      "8": '12',
+      "9": {
+        lat: 49.25120,
+        lng: 24.85975
+      },
+      "10": false
+    },
+    {
+      "1": 11,
+      "2": 'Task11',
+      "3": 'test',
+      "4": 'Smith',
+      "5": 'Standard',
+      "6": 'In progress',
+      "7": '03/28/2023 09:00:00',
+      "8": '99',
+      "9": {
+        lat: 49.36120,
+        lng: 24.72975
+      },
+      "10": false
+    },
+    {
+      "1": 12,
+      "2": 'Task12',
+      "3": 'test',
+      "4": 'John',
+      "5": 'Very important',
+      "6": 'Done',
+      "7": '03/28/2023 09:45:00',
+      "8": '12',
+      "9": {
+        lat: 49.46120,
+        lng: 24.61975
+      },
+      "10": false
+    },
+    {
+      "1": 13,
+      "2": 'Task14',
+      "3": 'test',
+      "4": 'Smith',
+      "5": 'Standard',
+      "6": 'In progress',
+      "7": '03/30/2023 09:00:00',
+      "8": '99',
+      "9": {
+        lat: 49.56120,
+        lng: 24.53975
+      },
+      "10": false
+    },
+    {
+      "1": 14,
+      "2": 'Task14',
+      "3": 'test',
+      "4": 'John',
+      "5": 'Very important',
+      "6": 'Done',
+      "7": '03/30/2023 09:45:00',
+      "8": '12',
+      "9": {
+        lat: 49.66120,
+        lng: 24.44975
+      },
+      "10": false
+    },
+    {
+      "1": 15,
+      "2": 'Task1',
+      "3": 'test',
+      "4": 'John',
+      "5": 'Very important',
+      "6": 'Done',
+      "7": '03/20/2023 08:30:00',
+      "8": '12',
+      "9": {
+        lat: 49.76120,
+        lng: 24.35975
+      },
+      "10": false
+    },
+    {
+      "1": 16,
+      "2": 'Task2',
+      "3": 'Lorem ipsum dolor sit amet consectetur. Tempus aliquam tortor ullamcorper vestibulum sit. Ac velit lectus quis non tortor scelerisque in velit.',
+      "4": 'Smith',
+      "5": 'Standard',
+      "6": 'In progress',
+      "7": '03/22/2023 10:45:00',
+      "8": '99',
+      "9": {
+        lat: 49.86120,
+        lng: 24.26975
+      },
+      "10": false
+    },
+    {
+      "1": 17,
+      "2": 'Task3',
+      "3": 'test',
+      "4": 'Smith',
+      "5": 'Standard',
+      "6": 'In progress',
+      "7": '03/24/2023 09:15:00',
+      "8": '99',
+      "9": {
+        lat: 49.96120,
+        lng: 24.17975
+      },
+      "10": false
+    },
+    {
+      "1": 18,
+      "2": 'Task4',
+      "3": 'test',
+      "4": 'John',
+      "5": 'Very important',
+      "6": 'Done',
+      "7": '03/24/2023 09:30:00',
+      "8": '12',
+      "9": {
+        lat: 49.07120,
+        lng: 24.92975
+      },
+      "10": false
+    },
+    {
+      "1": 19,
+      "2": 'Task5',
+      "3": 'test',
+      "4": 'Smith',
+      "5": 'Standard',
+      "6": 'In progress',
+      "7": '03/24/2023 09:00:00',
+      "8": '99',
+      "9": {
+        lat: 49.17120,
+        lng: 24.93975
+      },
+      "10": false
+    },
+    {
+      "1": 20,
+      "2": 'Task6',
+      "3": 'test',
+      "4": 'John',
+      "5": 'Very important',
+      "6": 'Testing',
+      "7": '03/24/2023 09:45:00',
+      "8": '12',
+      "9": {
+        lat: 49.27120,
+        lng: 24.84975
+      },
+      "10": false
+    },
+    {
+      "1": 21,
+      "2": 'Task7',
+      "3": 'test',
+      "4": 'John',
+      "5": 'Very important',
+      "6": 'Done',
+      "7": '03/27/2023 08:30:00',
+      "8": '12',
+      "9": {
+        lat: 49.37120,
+        lng: 24.75975
+      },
+      "10": false
+    },
+    {
+      "1": 22,
+      "2": 'Task5',
+      "3": 'test',
+      "4": 'Smith',
+      "5": 'Standard',
+      "6": 'In progress',
+      "7": '03/24/2023 09:00:00',
+      "8": '99',
+      "9": {
+        lat: 49.47120,
+        lng: 24.66975
+      },
+      "10": false
+    },
+    {
+      "1": 23,
+      "2": 'Task6',
+      "3": 'test',
+      "4": 'John',
+      "5": 'Very important',
+      "6": 'Testing',
+      "7": '03/24/2023 09:45:00',
+      "8": '12',
+      "9": {
+        lat: 49.57120,
+        lng: 24.57975
+      },
+      "10": false
+    },
+    {
+      "1": 24,
+      "2": 'Task7',
+      "3": 'test',
+      "4": 'John',
+      "5": 'Very important',
+      "6": 'Done',
+      "7": '03/27/2023 08:30:00',
+      "8": '12',
+      "9": {
+        lat: 49.67120,
+        lng: 24.48975
+      },
+      "10": false
+    },
+    {
+      "1": 25,
+      "2": 'Task6',
+      "3": 'test',
+      "4": 'John',
+      "5": 'Very important',
+      "6": 'Testing',
+      "7": '03/24/2023 09:45:00',
+      "8": '12',
+      "9": {
+        lat: 49.78120,
+        lng: 24.39975
+      },
+      "10": false
+    },
+    {
+      "1": 26,
+      "2": 'Task7',
+      "3": 'test',
+      "4": 'John',
+      "5": 'Very important',
+      "6": 'Done',
+      "7": '03/27/2023 08:30:00',
+      "8": '12',
+      "9": {
+        lat: 49.87120,
+        lng: 24.29975
+      },
+      "10": false
+    }
+  ]
+}
--- src/redux/actions/listFieldActions.ts
+++ src/redux/actions/listFieldActions.ts
@@ -12,11 +12,12 @@ any
 > => {
   return async (dispatch: Dispatch<any>) => {
     try {
-      const response = await fieldDefinitionService.getAllFieldDefinition(1);
-      if(response && response.code == 0)
-      {
-        dispatch(setColumns(response.result));
-      } 
+      // const response = await fieldDefinitionService.getAllFieldDefinition(1);
+      // if(response && response.code == 0)
+      // {
+      //   dispatch(setColumns(response.result));
+      // } 
+      dispatch(setColumns(getListFields()));
     } catch (error) {
      console.log('errr')
     }
@@ -36,3 +37,167 @@ export const setSorts = (sorts: any) => ({
   type: 'SET_SORTS',
   payload: sorts
 });
+
+const getListFields = () : any[] =>{
+   return [
+    {
+      id: '1',
+      name: 'ID',
+      icon: 'task',
+      type: 'id',
+      visible: true,
+      ordering: 1,
+      required: true,
+      detailsOnly:true,
+      description:"",
+      tableDefinitionId:1
+    },
+    {
+      id: '2',
+      name: 'Task Name',
+      icon: 'task',
+      type: 'Text',
+      visible: true,
+      ordering: 2,
+      required: true,
+      detailsOnly:true,
+      description:"",
+      tableDefinitionId:1
+    },
+    {
+      id: '3',
+      name: 'Description',
+      icon: 'task',
+      type: 'textarea',
+      visible: true,
+      ordering: 3,
+      required: true,
+      detailsOnly:true,
+      description:"",
+      tableDefinitionId:1
+    },
+    {
+      id: '4',
+      name: 'User',
+      icon: 'user',
+      type: 'avatar',
+      visible: true,
+      ordering: 4,
+      required: true,
+      detailsOnly:true,
+      description:"",
+      tableDefinitionId:1
+    },
+    {
+      id: '5',
+      name: 'Importance',
+      icon: 'importance',
+      type: 'choice',
+      ordering: 5,
+      required: true,
+      detailsOnly:true,
+      description:"",
+      tableDefinitionId:1,
+      choices: [
+        {
+          name: "Very important",
+          color: {
+            bg: '#FFB7B7',
+            fill: '#333'
+          }
+        },
+        {
+          name: "Standard",
+          color: {
+            bg: '#FFEBB7',
+            fill: '#333'
+          }
+        }
+      ],
+      visible: true
+    },
+    {
+      id: '6',
+      name: 'Phase',
+      icon: 'phase',
+      type: 'choice',
+      ordering: 6,
+      required: true,
+      detailsOnly:true,
+      description:"",
+      tableDefinitionId:1,
+      choices: [
+        {
+          name: "Done",
+          color: {
+            bg: '#B7FFBA',
+            fill: '#333'
+          }
+        },
+        {
+          name: "In progress",
+          color: {
+            bg: '#FFEBB7',
+            fill: '#333'
+          }
+        },
+        {
+          name: "Testing",
+          color: {
+            bg: '#FFEBB7',
+            fill: '#333'
+          }
+        }
+      ],
+      visible: true
+    },
+    {
+      id: '7',
+      name: 'Date',
+      icon: 'date',
+      type: 'date',
+      visible: true,
+      ordering: 7,
+      required: true,
+      detailsOnly:true,
+      description:"",
+      tableDefinitionId:1
+    },
+    {
+      id: '8',
+      name: 'Price',
+      icon: 'price',
+      type: 'floating',
+      visible: true,
+      ordering: 8,
+      required: true,
+      detailsOnly:true,
+      description:"",
+      tableDefinitionId:1
+    },
+    {
+      id: '9',
+      name: 'Location',
+      icon: 'location',
+      type: 'location',
+      visible: false,
+      ordering: 8,
+      required: true,
+      detailsOnly:true,
+      description:"",
+      tableDefinitionId:1
+    },
+    {
+      id: '10',
+      name: 'MapVisible',
+      icon: 'map',
+      type: 'boolean',
+      visible: false,
+      ordering: 8,
+      required: false,
+      detailsOnly:true,
+      description:"",
+      tableDefinitionId:1
+    }
+  ]
+}
