268c2304b7e09fa078eb6f2712c0269ee09f0bd3
Leonardo8221 <r.leonardo0802@gmail.com>
2023-06-07 10:33:11 +0200

    Settings page

---
+++ src/components/profile-settings/VerticalTabs.tsx
@@ -0,0 +1,260 @@
+import * as React from "react";
+
+import {
+  Box,
+  Typography,
+  Tabs,
+  Tab,
+  Avatar,
+  Button,
+  TextField,
+  Grid,
+} from "@mui/material";
+// ICONS----------------------------------------------
+import GroupsIcon from "@mui/icons-material/Groups";
+import SettingsIcon from "@mui/icons-material/Settings";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import GppGoodIcon from "@mui/icons-material/GppGood";
+import UploadIcon from "@mui/icons-material/Upload";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+      style={{ width: "100%" }}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Box>{children}</Box>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `vertical-tab-${index}`,
+    "aria-controls": `vertical-tabpanel-${index}`,
+  };
+}
+
+export default function VerticalTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box
+      sx={{
+        position: "absolute",
+        flexGrow: 1,
+        bgcolor: "background.paper",
+        display: "flex",
+        minHeight: "100%",
+        width: "100%",
+      }}
+    >
+      <Tabs
+        orientation="vertical"
+        variant="fullWidth"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        sx={{
+          borderRight: 1,
+          borderColor: "divider",
+          minHeight: "100%",
+          width: "25%",
+        }}
+      >
+        <Tab
+          label="Profile"
+          {...a11yProps(0)}
+          icon={<GroupsIcon sx={{ mr: 1 }} />}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "row",
+            justifyContent: "flex-start",
+          }}
+        />
+        <Tab
+          label="General"
+          {...a11yProps(1)}
+          icon={<SettingsIcon sx={{ mr: 1 }} />}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "row",
+            justifyContent: "flex-start",
+          }}
+        />
+        <Tab
+          label="Security"
+          {...a11yProps(2)}
+          icon={<GppGoodIcon sx={{ mr: 1 }} />}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "row",
+            justifyContent: "flex-start",
+          }}
+        />
+        <Tab
+          label="Notifications"
+          {...a11yProps(3)}
+          icon={<NotificationsIcon sx={{ mr: 1 }} />}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "row",
+            justifyContent: "flex-start",
+          }}
+        />
+        <Tab
+          label="Item Five"
+          {...a11yProps(4)}
+          icon={<GroupsIcon sx={{ mr: 1 }} />}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "row",
+            justifyContent: "flex-start",
+          }}
+        />
+        <Tab
+          label="Item Six"
+          {...a11yProps(5)}
+          icon={<GroupsIcon sx={{ mr: 1 }} />}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "row",
+            justifyContent: "flex-start",
+          }}
+        />
+        <Tab
+          label="Item Seven"
+          {...a11yProps(6)}
+          icon={<GroupsIcon sx={{ mr: 1 }} />}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "row",
+            justifyContent: "flex-start",
+          }}
+        />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <Typography variant="h4">Profile Settings</Typography>
+        <Box mt={4}>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Avatar
+              sx={{
+                width: 128,
+                height: 128,
+                border: "solid 4px #fff",
+                boxShadow: "0 0 24px 0 rgba(0,0,0,0.05)",
+                fontSize: 40,
+                position: "relative",
+                "&:hover .overlay": {
+                  cursor: "pointer",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "100%",
+                  height: "100%",
+                  position: "absolute",
+                },
+              }}
+            >
+              {/* CHANGE AVATAR v2 */}
+              {/* <Box
+                className="overlay"
+                sx={{ display: "none", backgroundColor: "rgba(0,0,0,0.5)" }}
+              >
+                <UploadIcon />
+                <Typography variant="body2">Upload image</Typography>
+              </Box> */}
+              FL
+            </Avatar>
+            <Box sx={{ display: "flex", flexDirection: "column", ml: 2 }}>
+              <Button variant="contained">Change Avatar</Button>
+              <Button variant="outlined" sx={{ mt: 1 }}>
+                Delete Avatar
+              </Button>
+            </Box>
+          </Box>
+          <Grid container spacing={2} sx={{ mt: 2 }}>
+            <Grid item md={6}>
+              <Typography variant="subtitle2">First Name</Typography>
+              <TextField sx={{ width: "100%" }} defaultValue="First Name" />
+            </Grid>
+            <Grid item md={6}>
+              <Typography variant="subtitle2">Last Name</Typography>
+              <TextField sx={{ width: "100%" }} defaultValue="Last Name" />
+            </Grid>
+          </Grid>
+          <Grid container spacing={2} sx={{ mt: 2 }}>
+            <Grid item xs={12}>
+              <Typography variant="subtitle2">Email</Typography>
+              <TextField
+                sx={{ width: "100%" }}
+                disabled
+                defaultValue="email@email.com"
+              />
+            </Grid>
+          </Grid>
+          <Grid container sx={{ mt: 2 }}>
+            <Grid item xs={12}>
+              <Typography variant="subtitle2">Bio</Typography>
+              <TextField
+                sx={{ width: "100%" }}
+                multiline
+                rows={8}
+                defaultValue="Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, dolor!"
+              />
+            </Grid>
+          </Grid>
+          <Button sx={{ mt: 2 }} disabled variant="contained">
+            Update Settings
+          </Button>
+        </Box>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        General Settings goes here
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Item Four
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        Item Five
+      </TabPanel>
+      <TabPanel value={value} index={5}>
+        Item Six
+      </TabPanel>
+      <TabPanel value={value} index={6}>
+        Item Seven
+      </TabPanel>
+    </Box>
+  );
+}
--- src/layouts/view/header/AccountPopover.tsx
+++ src/layouts/view/header/AccountPopover.tsx
@@ -1,25 +1,34 @@
-import { useState } from 'react';
-import { alpha } from '@mui/material/styles';
-import { Box, Divider, Typography, Stack, MenuItem, Avatar, IconButton, Popover } from '@mui/material';
-import account from '../../../_mock/account';
-import { useRouter } from 'next/router';
-import { authService } from 'src/services/auth.service';
+import { useState } from "react";
+import { alpha } from "@mui/material/styles";
+import {
+  Box,
+  Divider,
+  Typography,
+  Stack,
+  MenuItem,
+  Avatar,
+  IconButton,
+  Popover,
+} from "@mui/material";
+import account from "../../../_mock/account";
+import { useRouter } from "next/router";
+import { authService } from "src/services/auth.service";
 
 const MENU_OPTIONS = [
+  // {
+  //   label: 'Profile',
+  //   icon: 'eva:person-fill',
+  // },
   {
-    label: 'Profile',
-    icon: 'eva:person-fill',
-  },
-  {
-    label: 'Settings',
-    icon: 'eva:settings-2-fill',
+    label: "Settings",
+    icon: "eva:settings-2-fill",
   },
 ];
 
 export default function AccountPopover() {
   const [open, setOpen] = useState(null);
   const router = useRouter();
-  const handleOpen = (event:any) => {
+  const handleOpen = (event: any) => {
     setOpen(event.currentTarget);
   };
 
@@ -27,24 +36,24 @@ export default function AccountPopover() {
     setOpen(null);
   };
 
-  const logout = async() =>{
+  const logout = async () => {
     await authService.logout();
     router.push({
-     pathname: '/'
+      pathname: "/",
     });
-  }
- 
-  const style = (open && {
-    '&:before': {
+  };
+
+  const style = open && {
+    "&:before": {
       zIndex: 1,
       content: "''",
-      width: '100%',
-      height: '100%',
-      borderRadius: '50%',
-      position: 'absolute',
-      bgcolor: (theme:any) => alpha(theme.palette.grey[900], 0.8),
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      position: "absolute",
+      bgcolor: (theme: any) => alpha(theme.palette.grey[900], 0.8),
     },
-  });
+  };
 
   return (
     <>
@@ -52,26 +61,30 @@ export default function AccountPopover() {
         onClick={handleOpen}
         sx={{
           p: 0,
-          style
+          style,
         }}
       >
-        <Avatar sx={{ width: 30, height: 30 }} src={account.photoURL} alt="photoURL" />
+        <Avatar
+          sx={{ width: 30, height: 30 }}
+          src={account.photoURL}
+          alt="photoURL"
+        />
       </IconButton>
 
       <Popover
         open={Boolean(open)}
         anchorEl={open}
         onClose={handleClose}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
         PaperProps={{
           sx: {
             p: 0,
             mt: 1.5,
             ml: 0.75,
             width: 180,
-            '& .MuiMenuItem-root': {
-              typography: 'body2',
+            "& .MuiMenuItem-root": {
+              typography: "body2",
               borderRadius: 0.75,
             },
           },
@@ -81,12 +94,12 @@ export default function AccountPopover() {
           <Typography variant="subtitle2" noWrap>
             {account.displayName}
           </Typography>
-          <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
+          <Typography variant="body2" sx={{ color: "text.secondary" }} noWrap>
             {account.email}
           </Typography>
         </Box>
 
-        <Divider sx={{ borderStyle: 'dashed' }} />
+        <Divider sx={{ borderStyle: "dashed" }} />
 
         <Stack sx={{ p: 1 }}>
           {MENU_OPTIONS.map((option) => (
@@ -96,7 +109,7 @@ export default function AccountPopover() {
           ))}
         </Stack>
 
-        <Divider sx={{ borderStyle: 'dashed' }} />
+        <Divider sx={{ borderStyle: "dashed" }} />
 
         <MenuItem onClick={logout} sx={{ m: 1 }}>
           Logout
