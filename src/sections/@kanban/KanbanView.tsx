aa8c51040dbd4a1ee21a2f0f24caffb02a80031a
Leonardo8221 <r.leonardo0802@gmail.com>
2023-06-26 21:56:36 +0700

    update drag drop tasks in columns

--- src/sections/@kanban/KanbanView.tsx
+++ src/sections/@kanban/KanbanView.tsx
@@ -112,7 +112,6 @@ const KanbanView = ({currentView,columns, rows, open, setRows }: KanbanViewProps
         //   columnOrder: newColumnOrder
         // });
       } else {
-        console.log('aaaaaa')
         reorderRowMap(draggableId,source,destination)
       }
     }
@@ -120,80 +119,35 @@ const KanbanView = ({currentView,columns, rows, open, setRows }: KanbanViewProps
   const reorderRowMap = (draggableId:string,source:any,destination:any)=>{
      const destColumn = boardColumns.find((x)=>x.id.toString() === destination.droppableId);
      const destTasks = rows.filter((row: any) => row[kanbanConfig.boardColumnId] === destColumn?.id);
-     console.log(destTasks)
+     let destinationIndex = ((source.droppableId === destination.droppableId && destination.index>source.index)? destination.index+1:destination.index);
      let sourceIndex = 0;
      let destIndex = 0;
-     console.log(destination.index)
-     console.log(rows)
-     let newRows : any[] = []
-     if(source.droppableId !== destination.droppableId)
-     {
-      newRows = rows.map((row: any, index: number) => {
-        //update the column id of the dragged row
-        if (row.id.toString() === draggableId){
-          row[currentView.config?.boardColumnId] = destColumn?.id;
-        } 
-      
-        if (row.id === (destination.index<destTasks.length ?destTasks[destination.index]?.id:destTasks[destination.index-1]?.id))
-        {
-          //get the index of the row before which the dragged row needs to be inserted
-          destIndex = (destination.index<destTasks.length? index:index+1);
-        }
-        //get the index of the dragged row
-        if (row.id.toString() === draggableId)
-        {
-          sourceIndex = index;
-        } 
+     let newRows : any[] = rows.map((row: any, index: number) => {
+      //update the column id of the dragged row
+      if ((source.droppableId !== destination.droppableId)&& (row.id.toString() === draggableId)){
+        row[currentView.config?.boardColumnId] = destColumn?.id;
+      } 
+    
+      if (row.id === (destinationIndex<destTasks.length ?destTasks[destinationIndex]?.id:destTasks[destinationIndex-1]?.id))
+      {
+        //get the index of the row before which the dragged row needs to be inserted
+        destIndex = (destinationIndex<destTasks.length? index:index+1);
+      }
+      //get the index of the dragged row
+      if (row.id.toString() === draggableId)
+      {
+        sourceIndex = index;
+      } 
 
-        return row;
-      })
-     }
-     else
-     {
-      newRows = rows.map((row: any, index: number) => {
-        //if drag and drop is within the same column
-        //if drag from destination have index greater than source index
-        if(destination.index >= source.index)
-        {
-          if (row.id === (destination.index<destTasks.length-1?destTasks[destination.index+1]?.id:destTasks[destination.index]?.id))
-          {
-            //get the index of the row before which the dragged row needs to be inserted
-            destIndex = (destination.index<destTasks.length-1? index:index+1);
-          }
-        }
-        else
-        {
-          if (row.id === (destination.index<destTasks.length ?destTasks[destination.index]?.id:destTasks[destination.index-1]?.id))
-          {
-            //get the index of the row before which the dragged row needs to be inserted
-            destIndex = (destination.index<destTasks.length? index:index+1);
-          }
-        }
-        
-        //get the index of the dragged row
-        if (row.id.toString() === draggableId)
-        {
-          sourceIndex = index;
-        } 
-
-        return row;
-      })
-     }
-      
-        console.log(destIndex)
+      return row;
+    })
       //if source is before destination, then we need to decrement the destination index by 1
       if(sourceIndex<destIndex)
       {
         destIndex = destIndex-1;
       }
-      console.log(sourceIndex)
-      console.log(destIndex)
       const [changed] = newRows.splice(sourceIndex, 1);
-      console.log(newRows)
-      console.log(changed)
-      console.log(destIndex)
       newRows.splice(destIndex, 0, changed);
-      
       setRows(newRows);
   }
   const handleRowData = (row: any) => {
