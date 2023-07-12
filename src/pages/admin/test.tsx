5c64b24f269b37a1a5dfb772f469c2cdd2b8a770
Leonardo8221 <r.leonardo0802@gmail.com>
2023-07-12 10:52:01 +0200

    test with markdown

--- src/pages/admin/test.tsx
+++ src/pages/admin/test.tsx
@@ -5,46 +5,60 @@ import { getTranslations, getTranslation } from "src/utils/i18n";
 import { GetServerSideProps } from "next";
 import LanguagePopover from "src/layouts/LanguagePopover";
 import WysiwygView from "src/components/wysiwyg/wysiwygView";
-
+const markup = `
+# Manage your data in easy and flexible way.
+## Exciting News! Your Trusted Flexlists Product is About to Get a Whole Lot Better!
+* list1
+* list2
+* list3
+* list4
+* list5
+* list6 
+`
 type ContentTestProps = {
-    };
-const  ContentTest = ({ translations  }:ContentTestProps&{translations:TranslationText[]})=>{
-   
-    const t = (key:string):string=>{
-       return getTranslation(key,translations)
-        
+};
+const ContentTest = ({ translations }: ContentTestProps & { translations: TranslationText[] }) => {
+
+    const t = (key: string): string => {
+        return getTranslation(key, translations)
+
     }
     return (
         <>
-        <div>
-            <LanguagePopover/>
-        </div>
-        <div>
-            <h1>{t('Welcome')}</h1>
-            <WysiwygView
-              value={t('Description')}
-            />
-            {/* <div dangerouslySetInnerHTML={{ __html: t('Description') }} /> */}
-            <ReactMarkdown>{t('DescriptionMarkdown')}</ReactMarkdown>
-            <Box
-                component="img"
-                sx={{
-                height: 233,
-                width: 350,
-                maxHeight: { xs: 233, md: 167 },
-                maxWidth: { xs: 350, md: 250 },
-                }}
-                alt="no image"
-                src={t('ImageUrl')}
-            />
-        </div>
+            <div style={{ marginLeft: '100px' }}>
+                <ReactMarkdown>{markup}</ReactMarkdown>
+            </div>
+            <div>
+                <LanguagePopover />
+            </div>
+
+
+            <div>
+                <h1>{t('Welcome')}</h1>
+                <WysiwygView
+                    value={t('Description')}
+                />
+                {/* <div dangerouslySetInnerHTML={{ __html: t('Description') }} /> */}
+                <ReactMarkdown>{t('DescriptionMarkdown')}</ReactMarkdown>
+                <Box
+                    component="img"
+                    sx={{
+                        height: 233,
+                        width: 350,
+                        maxHeight: { xs: 233, md: 167 },
+                        maxWidth: { xs: 350, md: 250 },
+                    }}
+                    alt="no image"
+                    src={t('ImageUrl')}
+                />
+            </div>
         </>
-        
+
     )
 }
 // This gets called on every request
-export const getServerSideProps:GetServerSideProps = async (context) => {
-    
-      return await getTranslations("landing page",context)
-  }
+export const getServerSideProps: GetServerSideProps = async (context) => {
+
+    return await getTranslations("landing page", context)
+}
 export default ContentTest;
