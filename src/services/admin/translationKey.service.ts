525eef5d09e8b18fe447dc553549214a377306e1
Leonardo8221 <r.leonardo0802@gmail.com>
2023-07-21 13:51:19 +0200

    fixes for translations

--- src/services/admin/translationKey.service.ts
+++ src/services/admin/translationKey.service.ts
@@ -1,36 +1,36 @@
-import { FlexlistsError,FlexlistsSuccess } from "src/models/ApiResponse";
+import { FlexlistsError, FlexlistsSuccess } from "src/models/ApiResponse";
 import axios from "src/utils/axios";
 import { TranslationKeyType } from 'src/enums/SharedEnums'
 import { TranslationKeyDto } from 'src/models/TranslationKeyDto'
 
 export const translationKeyService = {
-    createTranslationKey,
-    updateTranslationKey,
-    getTranslationKeyById,
-    getAllTranslationKey,
-    deleteTranslationKey
+  createTranslationKey,
+  updateTranslationKey,
+  getTranslationKeyById,
+  getAllTranslationKey,
+  deleteTranslationKey
 };
 
-async function createTranslationKey(name:string,type:TranslationKeyType,ownerId:number,config:string): Promise<FlexlistsError|FlexlistsSuccess<TranslationKeyDto>> {
-  var response = await axios.post<FlexlistsError|FlexlistsSuccess<TranslationKeyDto>>(`/api/translationKey/createTranslationKey`, {name,type,ownerId,config})
-
-  return response.data;
-};
-async function updateTranslationKey(id:number,name:string,type:TranslationKeyType,ownerId:number,config:string): Promise<FlexlistsError|FlexlistsSuccess<TranslationKeyDto>> {
-  var response = await axios.post<FlexlistsError|FlexlistsSuccess<TranslationKeyDto>>(`/api/translationKey/updateTranslationKey`, {id,name,type,ownerId,config})
+async function createTranslationKey(name: string, type: TranslationKeyType, ownerId: number, config: string): Promise<FlexlistsError | FlexlistsSuccess<TranslationKeyDto>> {
+  var response = await axios.post<FlexlistsError | FlexlistsSuccess<TranslationKeyDto>>(`/api/translationKey/createTranslationKey`, { name, type, ownerId, config })
 
   return response.data;
 };
-async function getTranslationKeyById(id:number): Promise<FlexlistsError|FlexlistsSuccess<TranslationKeyDto>> {
-  var response = await axios.get<FlexlistsError|FlexlistsSuccess<TranslationKeyDto>>('/api/translationKey/getTranslationKeyById'+'?id='+id)
+async function updateTranslationKey(id: number, name: string, type: TranslationKeyType, ownerId: number, reusable: boolean, config: string): Promise<FlexlistsError | FlexlistsSuccess<TranslationKeyDto>> {
+  var response = await axios.post<FlexlistsError | FlexlistsSuccess<TranslationKeyDto>>(`/api/translationKey/updateTranslationKey`, { id, name, type, ownerId, reusable, config })
+
   return response.data;
 };
-async function getAllTranslationKey(): Promise<FlexlistsError|FlexlistsSuccess<TranslationKeyDto[]>> {
-  var response = await axios.get<FlexlistsError|FlexlistsSuccess<TranslationKeyDto[]>>('/api/translationKey/getAllTranslationKey')
+async function getTranslationKeyById(id: number): Promise<FlexlistsError | FlexlistsSuccess<TranslationKeyDto>> {
+  var response = await axios.get<FlexlistsError | FlexlistsSuccess<TranslationKeyDto>>('/api/translationKey/getTranslationKeyById' + '?id=' + id)
   return response.data;
 };
-async function deleteTranslationKey(id:number): Promise<FlexlistsError|FlexlistsSuccess<TranslationKeyDto>> {
-  var response = await axios.delete<FlexlistsError|FlexlistsSuccess<TranslationKeyDto>>(`/api/translationKey/deleteTranslationKey`+'?id='+id);
+async function getAllTranslationKey(): Promise<FlexlistsError | FlexlistsSuccess<TranslationKeyDto[]>> {
+  var response = await axios.get<FlexlistsError | FlexlistsSuccess<TranslationKeyDto[]>>('/api/translationKey/getAllTranslationKey?query=' + JSON.stringify({ where: { reusable: true } }))
+  return response.data;
+};
+async function deleteTranslationKey(id: number): Promise<FlexlistsError | FlexlistsSuccess<TranslationKeyDto>> {
+  var response = await axios.delete<FlexlistsError | FlexlistsSuccess<TranslationKeyDto>>(`/api/translationKey/deleteTranslationKey` + '?id=' + id);
 
   return response.data;
 };
