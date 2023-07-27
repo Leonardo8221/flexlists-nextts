9fa99403f2bbfcfa3f6944606dd90c729f1e2c1a
Leonardo8221 <r.leonardo0802@gmail.com>
2023-07-27 13:40:00 +0200

    markdown editor fix

--- src/components/rowedit/MarkdownEditor.tsx
+++ src/components/rowedit/MarkdownEditor.tsx
@@ -1,54 +1,91 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, TextField } from "@mui/material";
 import ReactMarkdown from "react-markdown";
 import "easymde/dist/easymde.min.css";
-import dynamic from 'next/dynamic'
-import remarkGfm from 'remark-gfm'
+import dynamic from "next/dynamic";
+import remarkGfm from "remark-gfm";
 
 const SimpleMdeReact = dynamic(
-    () => {
-        return import("react-simplemde-editor");
-    },
-    { ssr: false }
+  () => {
+    return import("react-simplemde-editor");
+  },
+  { ssr: false }
 );
 
-const MarkdownEditor = ({ id, name, value, handleChange, preview }: { id: number, name: string, value: string, handleChange: (newValue: string) => void, preview?: boolean }) => {
-
-    return !preview ? (
-        <Box
-            key={id}
-            sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "flex-start",
-                position: "relative",
-            }}
-        >
-            <Typography
-                variant="body1"
-                sx={{
-                    textTransform: "capitalize",
-                    color: "rgba(0, 0, 0, 0.6)",
-                    fontSize: "12px",
-                    position: "absolute",
-                    top: "-10px",
-                    left: "12px",
-                    background: "#fff",
-                }}
-            >
-                {name}
-            </Typography>
-            <SimpleMdeReact value={value}
-                style={{ width: '100%' }}
-                onChange={handleChange} />
-        </Box>
-    ) : (
-        <div key={id}>
-            <Typography variant="subtitle2" sx={{ textTransform: "uppercase" }}>
-                {name}
-            </Typography>
-            <ReactMarkdown remarkPlugins={[[remarkGfm, { singleTilde: false }]]}>{value}</ReactMarkdown>
-        </div>
-    );
-
-}
-export default MarkdownEditor
+const MarkdownEditor = ({
+  id,
+  name,
+  value,
+  handleChange,
+  preview,
+}: {
+  id: number;
+  name: string;
+  value: string;
+  handleChange: (newValue: string) => void;
+  preview?: boolean;
+}) => {
+  return !preview ? (
+    <Box
+      key={id}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        position: "relative",
+      }}
+    >
+      <Typography
+        variant="body1"
+        sx={{
+          textTransform: "capitalize",
+          color: "rgba(0, 0, 0, 0.6)",
+          fontSize: "12px",
+          position: "absolute",
+          top: "-10px",
+          left: "12px",
+          background: "#fff",
+        }}
+      >
+        {name}
+      </Typography>
+      <SimpleMdeReact
+        value={value}
+        style={{ width: "100%" }}
+        onChange={handleChange}
+      />
+    </Box>
+  ) : (
+    <div
+      key={id}
+      style={{
+        border: "1px solid rgba(158, 158, 158, 0.32)",
+        paddingBlock: 8,
+        paddingInline: 8,
+        position: "relative",
+      }}
+    >
+      <Typography
+        variant="body2"
+        component={"label"}
+        sx={{
+          textTransform: "capitalize",
+          fontSize: 12,
+          position: "absolute",
+          top: "-10px",
+          background: "#fff",
+          zIndex: 2,
+          px: 0.5,
+          color: "rgba(0, 0, 0, 0.6)",
+        }}
+      >
+        {name}
+      </Typography>
+      <div className="markdownWrapper">
+        <ReactMarkdown remarkPlugins={[[remarkGfm, { singleTilde: false }]]}>
+          {value}
+        </ReactMarkdown>
+      </div>
+    </div>
+  );
+};
+export default MarkdownEditor;
