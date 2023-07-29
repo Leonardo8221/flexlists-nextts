54e49ab531296a1efc29e58b1de8b27a7ae2c073
Leonardo8221 <r.leonardo0802@gmail.com>
2023-07-29 20:47:26 +0200

    build works again

--- src/services/list.service.ts
+++ src/services/list.service.ts
@@ -1,106 +1,106 @@
-import { FlexlistsError,FlexlistsSuccess } from "src/models/ApiResponse";
+import { FlexlistsError, FlexlistsSuccess } from "src/models/ApiResponse";
 import axios from "src/utils/axios";
 import { CreateListOutputDto } from 'src/models/ApiOutputModels'
 import { ListCategory } from 'src/enums/SharedEnums'
 import { ViewType } from 'src/enums/SharedEnums'
-import { PublishListOutputDto } from 'src/models/ApiOutputModels'
+//import { PublishListOutputDto } from 'src/models/ApiOutputModels'
 import { PublishType } from 'src/enums/SharedEnums'
 import { List } from 'src/models/SharedModels'
 import { Role } from 'src/enums/SharedEnums'
-import { AddAccessToListOutputDto } from 'src/models/ApiOutputModels'
-import { GetKeysOutputDto } from 'src/models/ApiOutputModels'
+//import { AddAccessToListOutputDto } from 'src/models/ApiOutputModels'
+//import { GetKeysOutputDto } from 'src/models/ApiOutputModels'
 
 export const listService = {
-    createList,
-    deleteList,
-    archiveList,
-    unArchiveList,
-    publishList,
-    shareList,
-    undoList,
-    truncateList,
-    getLists,
-    getList,
-    copyList,
-    addUserToList,
-    removeUserFromList,
-    addAccessToList,
-    removeAccessFromList,
-    getKeys,
+  createList,
+  deleteList,
+  archiveList,
+  unArchiveList,
+  //publishList,
+  shareList,
+  undoList,
+  truncateList,
+  getLists,
+  getList,
+  copyList,
+  addUserToList,
+  removeUserFromList,
+  //addAccessToList,
+  removeAccessFromList,
+  //getKeys,
 };
 
-async function createList(name:string,description?:string,category?:ListCategory,type?:ViewType): Promise<FlexlistsError|FlexlistsSuccess<CreateListOutputDto>> {
-  var response = await axios.post<FlexlistsError|FlexlistsSuccess<CreateListOutputDto>>(`/api/list/createList`, {name,description,category,type})
-
-  return response.data;
-};
-async function deleteList(id:number): Promise<FlexlistsError|FlexlistsSuccess> {
-  var response = await axios.delete<FlexlistsError|FlexlistsSuccess>(`/api/list/deleteList`+`?id=${id}`);
+async function createList(name: string, description?: string, category?: ListCategory, type?: ViewType): Promise<FlexlistsError | FlexlistsSuccess<CreateListOutputDto>> {
+  var response = await axios.post<FlexlistsError | FlexlistsSuccess<CreateListOutputDto>>(`/api/list/createList`, { name, description, category, type })
 
   return response.data;
 };
-async function archiveList(id:number): Promise<FlexlistsError|FlexlistsSuccess> {
-  var response = await axios.post<FlexlistsError|FlexlistsSuccess>(`/api/list/archiveList`, {id})
+async function deleteList(id: number): Promise<FlexlistsError | FlexlistsSuccess> {
+  var response = await axios.delete<FlexlistsError | FlexlistsSuccess>(`/api/list/deleteList` + `?id=${id}`);
 
   return response.data;
 };
-async function unArchiveList(id:number): Promise<FlexlistsError|FlexlistsSuccess> {
-  var response = await axios.post<FlexlistsError|FlexlistsSuccess>(`/api/list/unArchiveList`, {id})
+async function archiveList(id: number): Promise<FlexlistsError | FlexlistsSuccess> {
+  var response = await axios.post<FlexlistsError | FlexlistsSuccess>(`/api/list/archiveList`, { id })
 
   return response.data;
 };
-async function publishList(listId:number,type:PublishType,name?:string): Promise<FlexlistsError|FlexlistsSuccess<PublishListOutputDto>> {
-  var response = await axios.post<FlexlistsError|FlexlistsSuccess<PublishListOutputDto>>(`/api/list/publishList`, {listId,type,name})
+async function unArchiveList(id: number): Promise<FlexlistsError | FlexlistsSuccess> {
+  var response = await axios.post<FlexlistsError | FlexlistsSuccess>(`/api/list/unArchiveList`, { id })
 
   return response.data;
 };
-async function shareList(): Promise<FlexlistsError|FlexlistsSuccess> {
-  var response = await axios.get<FlexlistsError|FlexlistsSuccess>('/api/list/shareList')
+// async function publishList(listId:number,type:PublishType,name?:string): Promise<FlexlistsError|FlexlistsSuccess<PublishListOutputDto>> {
+//   var response = await axios.post<FlexlistsError|FlexlistsSuccess<PublishListOutputDto>>(`/api/list/publishList`, {listId,type,name})
+
+//   return response.data;
+// };
+async function shareList(): Promise<FlexlistsError | FlexlistsSuccess> {
+  var response = await axios.get<FlexlistsError | FlexlistsSuccess>('/api/list/shareList')
   return response.data;
 };
-async function undoList(): Promise<FlexlistsError|FlexlistsSuccess> {
-  var response = await axios.get<FlexlistsError|FlexlistsSuccess>('/api/list/undoList')
+async function undoList(): Promise<FlexlistsError | FlexlistsSuccess> {
+  var response = await axios.get<FlexlistsError | FlexlistsSuccess>('/api/list/undoList')
   return response.data;
 };
-async function truncateList(id:number): Promise<FlexlistsError|FlexlistsSuccess> {
-  var response = await axios.delete<FlexlistsError|FlexlistsSuccess>(`/api/list/truncateList`+`?id=${id}`);
+async function truncateList(id: number): Promise<FlexlistsError | FlexlistsSuccess> {
+  var response = await axios.delete<FlexlistsError | FlexlistsSuccess>(`/api/list/truncateList` + `?id=${id}`);
 
   return response.data;
 };
-async function getLists(): Promise<FlexlistsError|FlexlistsSuccess<List[]>> {
-  var response = await axios.get<FlexlistsError|FlexlistsSuccess<List[]>>('/api/list/getLists')
+async function getLists(): Promise<FlexlistsError | FlexlistsSuccess<List[]>> {
+  var response = await axios.get<FlexlistsError | FlexlistsSuccess<List[]>>('/api/list/getLists')
   return response.data;
 };
-async function getList(listId:number): Promise<FlexlistsError|FlexlistsSuccess<List>> {
-  var response = await axios.get<FlexlistsError|FlexlistsSuccess<List>>('/api/list/getList'+`?listId=${listId}`)
+async function getList(listId: number): Promise<FlexlistsError | FlexlistsSuccess<List>> {
+  var response = await axios.get<FlexlistsError | FlexlistsSuccess<List>>('/api/list/getList' + `?listId=${listId}`)
   return response.data;
 };
-async function copyList(id:number): Promise<FlexlistsError|FlexlistsSuccess> {
-  var response = await axios.post<FlexlistsError|FlexlistsSuccess>(`/api/list/copyList`, {id})
+async function copyList(id: number): Promise<FlexlistsError | FlexlistsSuccess> {
+  var response = await axios.post<FlexlistsError | FlexlistsSuccess>(`/api/list/copyList`, { id })
 
   return response.data;
 };
-async function addUserToList(listId:number,userId:number,role:Role): Promise<FlexlistsError|FlexlistsSuccess> {
-  var response = await axios.post<FlexlistsError|FlexlistsSuccess>(`/api/list/addUserToList`, {listId,userId,role})
+async function addUserToList(listId: number, userId: number, role: Role): Promise<FlexlistsError | FlexlistsSuccess> {
+  var response = await axios.post<FlexlistsError | FlexlistsSuccess>(`/api/list/addUserToList`, { listId, userId, role })
 
   return response.data;
 };
-async function removeUserFromList(listId:number,userId:number): Promise<FlexlistsError|FlexlistsSuccess> {
-  var response = await axios.delete<FlexlistsError|FlexlistsSuccess>(`/api/list/removeUserFromList`+`?listId=${listId}&userId=${userId}`);
+async function removeUserFromList(listId: number, userId: number): Promise<FlexlistsError | FlexlistsSuccess> {
+  var response = await axios.delete<FlexlistsError | FlexlistsSuccess>(`/api/list/removeUserFromList` + `?listId=${listId}&userId=${userId}`);
 
   return response.data;
 };
-async function addAccessToList(listId:number,role:number,name?:string): Promise<FlexlistsError|FlexlistsSuccess<AddAccessToListOutputDto>> {
-  var response = await axios.post<FlexlistsError|FlexlistsSuccess<AddAccessToListOutputDto>>(`/api/list/addAccessToList`, {listId,role,name})
+// async function addAccessToList(listId:number,role:number,name?:string): Promise<FlexlistsError|FlexlistsSuccess<AddAccessToListOutputDto>> {
+//   var response = await axios.post<FlexlistsError|FlexlistsSuccess<AddAccessToListOutputDto>>(`/api/list/addAccessToList`, {listId,role,name})
+
+//   return response.data;
+// };
+async function removeAccessFromList(listId: number, keyId: number): Promise<FlexlistsError | FlexlistsSuccess> {
+  var response = await axios.delete<FlexlistsError | FlexlistsSuccess>(`/api/list/removeAccessFromList` + `?listId=${listId}&keyId=${keyId}`);
 
   return response.data;
 };
-async function removeAccessFromList(listId:number,keyId:number): Promise<FlexlistsError|FlexlistsSuccess> {
-  var response = await axios.delete<FlexlistsError|FlexlistsSuccess>(`/api/list/removeAccessFromList`+`?listId=${listId}&keyId=${keyId}`);
-
-  return response.data;
-};
-async function getKeys(listId:number): Promise<FlexlistsError|FlexlistsSuccess<GetKeysOutputDto[]>> {
-  var response = await axios.get<FlexlistsError|FlexlistsSuccess<GetKeysOutputDto[]>>('/api/list/getKeys'+`?listId=${listId}`)
-  return response.data;
-};
+// async function getKeys(listId:number): Promise<FlexlistsError|FlexlistsSuccess<GetKeysOutputDto[]>> {
+//   var response = await axios.get<FlexlistsError|FlexlistsSuccess<GetKeysOutputDto[]>>('/api/list/getKeys'+`?listId=${listId}`)
+//   return response.data;
+// };
