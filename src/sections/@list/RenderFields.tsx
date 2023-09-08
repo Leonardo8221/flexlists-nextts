44a559da891274ead740600cc8a52feffa97c9c4
Leonardo8221 <r.leonardo0802@gmail.com>
2023-09-08 15:29:36 +0800

    implement sublist with breadcrumb

--- src/sections/@list/RenderFields.tsx
+++ src/sections/@list/RenderFields.tsx
@@ -8,7 +8,6 @@ import {
   FormGroup,
 } from "@mui/material";
 import { connect } from "react-redux";
-import { fetchRows, setCurrentView } from "../../redux/actions/viewActions";
 import useResponsive from "../../hooks/useResponsive";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
@@ -34,6 +33,7 @@ import UploadButton from "src/components/upload/UploadButton";
 import ReactPlayer from "react-player";
 import ColorPicker from "src/components/color-picker/ColorPicker";
 import LookupField from "src/components/relation/LookupField";
+import SublistField from "src/components/relation/SublistField";
 
 type RenderFieldProps = {
   column: ViewField;
@@ -1059,19 +1059,17 @@ const RenderField = ({
       return (
         <LookupField column={column} isPrint={isPrint} currentMode={currentMode} values={values} submit={submit} setValues={setValues} />
       );
+    case FieldUiTypeEnum.Sublist:
+      return (
+        <SublistField column={column} isPrint={isPrint} currentMode={currentMode} values={values} submit={submit} setValues={setValues} />
+      );
     default:
       return <div key={column.id}></div>;
   };
 };
 
 const mapStateToProps = (state: any) => ({
-  columns: state.view.columns,
-  currentView: state.view.currentView
+  columns: state.view.columns
 });
 
-const mapDispatchToProps = {
-  setCurrentView,
-  fetchRows,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(RenderField);
+export default connect(mapStateToProps)(RenderField);
