558d7c2a59f37130140d2fc567bd27861e1cb643
Leonardo8221 <r.leonardo0802@gmail.com>
2023-10-07 06:41:20 +0200

    reorder contents fixes and board to board

--- src/sections/@kanban/KanbanView.tsx
+++ src/sections/@kanban/KanbanView.tsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
-import { connect } from 'react-redux';
-import { fetchRows, setCurrentView } from '../../redux/actions/viewActions';
-import KanbanColumn from './KanbanColumn';
-import { DragDropContext, Droppable } from 'react-beautiful-dnd';
-import { styled } from '@mui/material/styles';
-import ViewFooter from '../../components/view-footer/ViewFooter';
+import { connect } from "react-redux";
+import { fetchRows, setCurrentView } from "../../redux/actions/viewActions";
+import KanbanColumn from "./KanbanColumn";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import { styled } from "@mui/material/styles";
+import ViewFooter from "../../components/view-footer/ViewFooter";
 import { ChoiceModel } from "src/models/ChoiceModel";
 import { FlatWhere, View } from "src/models/SharedModels";
 import { KanbanConfig } from "src/models/ViewConfig";
@@ -14,11 +14,11 @@ import { FlexlistsError, isSucc } from "src/models/ApiResponse";
 import { setFlashMessage } from "src/redux/actions/authAction";
 import { TranslationText } from "src/models/SharedModels";
 import { getTranslation } from "src/utils/i18n";
-import Head from 'next/head';
+import Head from "next/head";
 
 type KanbanViewProps = {
   columns: ViewField[];
-  currentView:View;
+  currentView: View;
   rows: any[];
   open: boolean;
   translations: TranslationText[];
@@ -28,25 +28,42 @@ type KanbanViewProps = {
   clearRefresh: () => void;
 };
 
-const KanbanView = ({ translations, currentView,columns, rows, open, refresh, fetchRows, setCurrentView, clearRefresh }: KanbanViewProps) => {
+const KanbanView = ({
+  translations,
+  currentView,
+  columns,
+  rows,
+  open,
+  refresh,
+  fetchRows,
+  setCurrentView,
+  clearRefresh,
+}: KanbanViewProps) => {
   const t = (key: string): string => {
     return getTranslation(key, translations);
   };
   const [visibleAddRowPanel, setVisibleAddRowPanel] = useState(false);
   const [rowData, setRowData] = useState(null);
   const [windowHeight, setWindowHeight] = useState(0);
-  const [boardColumns, setBoardColumns] = useState<ChoiceModel[]>(columns.find((x)=>x.id === currentView.config?.boardColumnId)?.config?.values as ChoiceModel[]);
-  const [kanbanConfig,setKanbanConfig] = useState<KanbanConfig>(currentView.config as KanbanConfig);
-  const [mode, setMode] = useState<"view" | "create" | "update" | "comment">("view");
+  const [boardColumns, setBoardColumns] = useState<ChoiceModel[]>(
+    columns.find((x) => x.id === currentView.config?.boardColumnId)?.config
+      ?.values as ChoiceModel[]
+  );
+  const [kanbanConfig, setKanbanConfig] = useState<KanbanConfig>(
+    currentView.config as KanbanConfig
+  );
+  const [mode, setMode] = useState<"view" | "create" | "update" | "comment">(
+    "view"
+  );
 
-  const Container = styled('div')(({ theme }) => ({
-    display: 'flex',
-    padding: '16px',
+  const Container = styled("div")(({ theme }) => ({
+    display: "flex",
+    padding: "16px",
     height: `${windowHeight - (open ? 279 : 235)}px`,
-    overflow: 'auto',
-    [theme.breakpoints.up('md')]: {
+    overflow: "auto",
+    [theme.breakpoints.up("md")]: {
       height: `${windowHeight - 193}px`,
-    }
+    },
   }));
 
   useEffect(() => {
@@ -63,24 +80,24 @@ const KanbanView = ({ translations, currentView,columns, rows, open, refresh, fe
     let newView: View = Object.assign({}, currentView);
     newView.conditions = [];
 
-    for(let i = 0; i < boardColumns.length; i++) {
+    for (let i = 0; i < boardColumns.length; i++) {
       const filter: FlatWhere = {
         left: kanbanConfig.boardColumnId,
         leftType: "Field",
         right: boardColumns[i].id,
         rightType: "SearchString",
-        cmp: 'eq',
+        cmp: "eq",
       } as FlatWhere;
-  
+
       newView.conditions.push(filter);
-      if (i < boardColumns.length -1) newView.conditions.push("Or");
+      if (i < boardColumns.length - 1) newView.conditions.push("Or");
     }
-    
+
     setCurrentView(newView);
     fetchRows();
   }, []);
 
-  const onDragEnd = (result: any) => {
+  const onDragEnd = async (result: any) => {
     if (!result.destination) return;
     const { source, destination, draggableId } = result;
     if (
@@ -90,29 +107,82 @@ const KanbanView = ({ translations, currentView,columns, rows, open, refresh, fe
       return;
     }
 
-    const destColumn = boardColumns.find((x) => x.id.toString() === destination.droppableId);
-    const destTasks = rows.filter((row: any) => row[kanbanConfig.boardColumnId] === destColumn?.id);
+    const destColumn = boardColumns.find(
+      (x) => x.id.toString() === destination.droppableId
+    );
+    const destTasks = rows.filter(
+      (row: any) => row[kanbanConfig.boardColumnId] === destColumn?.id
+    );
+
+    //console.log("source, dest", source.index, destination.index, destTasks);
+    let targetId: number | undefined = undefined;
 
     if (source.droppableId !== destination.droppableId) {
+      console.log("moved to another board");
       // update content with destination board
+      targetId =
+        destTasks[destination.index]?.id ??
+        (destTasks && destTasks.length > 0
+          ? destTasks[destTasks.length - 1].id
+          : null);
+      // move to another board;
+      const updateRowRespone = await listContentService.updateContents(
+        currentView.id,
+        [
+          {
+            id: parseInt(draggableId),
+            [kanbanConfig.boardColumnId]: destColumn?.id,
+          },
+        ]
+      );
+      if (targetId) {
+        // console.log(
+        //   destination.index,
+        //   parseInt(draggableId),
+        //   targetId
+        //   //destTasks[destination.index].id
+        // );
+        await reorderContents(parseInt(draggableId), targetId);
+      }
+    } else {
+      // console.log(
+      //   destination.index,
+      //   parseInt(draggableId),
+      //   targetId,
+      //   destTasks[destination.index].id
+      // );
+      if (destination.index && destination.index > 0) {
+        if (source.index > destination.index) {
+          targetId = destTasks[destination.index - 1].id;
+        } else {
+          targetId = destTasks[destination.index].id;
+        }
+      }
+      await reorderContents(
+        parseInt(draggableId),
+        targetId
+        //destTasks[destination.index].id
+      );
     }
-
-    reorderContents(parseInt(draggableId), destTasks[destination.index].id);
+    fetchRows();
   };
 
-  const reorderContents = async (sourceContentId: number, targetContentId: number) => {
+  const reorderContents = async (
+    sourceContentId: number,
+    targetContentId?: number
+  ) => {
     const reorderContentResponse = await listContentService.reordercontents(
       currentView.id,
       sourceContentId,
       targetContentId
     );
-    
+
     if (isSucc(reorderContentResponse)) {
       setFlashMessage({
         message: "Reordered successfully",
         type: "success",
       });
-      fetchRows();
+
       return;
     } else {
       setFlashMessage({
@@ -137,20 +207,52 @@ const KanbanView = ({ translations, currentView,columns, rows, open, refresh, fe
         <meta name="keywords" content={t("Kanban Meta Keywords")} />
       </Head>
       <Droppable droppableId="board" direction="horizontal" type="column">
-        {(provided: any) => (
-          provided.droppableProps && <Container ref={provided.innerRef} {...provided.droppableProps} className="board">
-            {boardColumns && boardColumns.length>0 && boardColumns.map((boardColumn: any, index: number) => {
-              // const column = testData.columns[columnId];
-              const tasks = rows.filter((row: any) => row[kanbanConfig.boardColumnId] === boardColumn.id);
-              
-              return <KanbanColumn translations={translations} key={boardColumn.id} kanbanConfig={kanbanConfig} column={boardColumn} tasks={tasks} index={index} openNewRowPanel={() => { setVisibleAddRowPanel(true); }} handleRowData={handleRowData} />;
-            })}
-            {provided.placeholder}
-          </Container>
-        )}
+        {(provided: any) =>
+          provided.droppableProps && (
+            <Container
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              className="board"
+            >
+              {boardColumns &&
+                boardColumns.length > 0 &&
+                boardColumns.map((boardColumn: any, index: number) => {
+                  // const column = testData.columns[columnId];
+                  const tasks = rows.filter(
+                    (row: any) =>
+                      row[kanbanConfig.boardColumnId] === boardColumn.id
+                  );
+
+                  return (
+                    <KanbanColumn
+                      translations={translations}
+                      key={boardColumn.id}
+                      kanbanConfig={kanbanConfig}
+                      column={boardColumn}
+                      tasks={tasks}
+                      index={index}
+                      openNewRowPanel={() => {
+                        setVisibleAddRowPanel(true);
+                      }}
+                      handleRowData={handleRowData}
+                    />
+                  );
+                })}
+              {provided.placeholder}
+            </Container>
+          )
+        }
       </Droppable>
-      
-      <ViewFooter visibleAddRowPanel={visibleAddRowPanel} rowData={rowData} setVisibleAddRowPanel={setVisibleAddRowPanel} setRowData={setRowData} translations={translations} mode={mode} setMode={setMode} />
+
+      <ViewFooter
+        visibleAddRowPanel={visibleAddRowPanel}
+        rowData={rowData}
+        setVisibleAddRowPanel={setVisibleAddRowPanel}
+        setRowData={setRowData}
+        translations={translations}
+        mode={mode}
+        setMode={setMode}
+      />
     </DragDropContext>
   );
 };
@@ -158,12 +260,12 @@ const KanbanView = ({ translations, currentView,columns, rows, open, refresh, fe
 const mapStateToProps = (state: any) => ({
   rows: state.view.rows,
   columns: state.view.columns,
-  currentView: state.view.currentView
+  currentView: state.view.currentView,
 });
 
 const mapDispatchToProps = {
   fetchRows,
-  setCurrentView
+  setCurrentView,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(KanbanView);
--- src/services/listContent.service.ts
+++ src/services/listContent.service.ts
@@ -1,12 +1,12 @@
 import { FlexlistsError, FlexlistsSuccess } from "src/models/ApiResponse";
 import axios from "src/utils/axios";
-import { CreateContentOutputDto } from 'src/models/ApiOutputModels'
-import { CloneContentOutputDto } from 'src/models/ApiOutputModels'
-import { SearchContentsOutputDto } from 'src/models/ApiOutputModels'
-import { Sort } from 'src/models/SharedModels'
-import { SearchOutputDto } from 'src/models/ApiOutputModels'
-import { SearchType } from 'src/enums/SharedEnums'
-import { Query } from 'src/models/SharedModels'
+import { CreateContentOutputDto } from "src/models/ApiOutputModels";
+import { CloneContentOutputDto } from "src/models/ApiOutputModels";
+import { SearchContentsOutputDto } from "src/models/ApiOutputModels";
+import { Sort } from "src/models/SharedModels";
+import { SearchOutputDto } from "src/models/ApiOutputModels";
+import { SearchType } from "src/enums/SharedEnums";
+import { Query } from "src/models/SharedModels";
 
 export const listContentService = {
   reordercontents,
@@ -23,72 +23,187 @@ export const listContentService = {
   search,
 };
 
-export async function reordercontents(viewId: number, sourceContentId: number, targetContentId: number): Promise<FlexlistsError | FlexlistsSuccess> {
-  var response = await axios.post<FlexlistsError | FlexlistsSuccess>(`/api/listContent/reorderContents`, { viewId, sourceContentId, targetContentId })
+export async function reordercontents(
+  viewId: number,
+  sourceContentId: number,
+  targetContentId?: number
+): Promise<FlexlistsError | FlexlistsSuccess> {
+  var response = await axios.post<FlexlistsError | FlexlistsSuccess>(
+    `/api/listContent/reorderContents`,
+    { viewId, sourceContentId, targetContentId }
+  );
 
   return response.data;
-};
-export async function createContent(viewId: number, content: any): Promise<FlexlistsError | FlexlistsSuccess<CreateContentOutputDto>> {
-  var response = await axios.post<FlexlistsError | FlexlistsSuccess<CreateContentOutputDto>>(`/api/listContent/createContent`, { viewId, content })
+}
+export async function createContent(
+  viewId: number,
+  content: any
+): Promise<FlexlistsError | FlexlistsSuccess<CreateContentOutputDto>> {
+  var response = await axios.post<
+    FlexlistsError | FlexlistsSuccess<CreateContentOutputDto>
+  >(`/api/listContent/createContent`, { viewId, content });
 
   return response.data;
-};
-export async function cloneContent(viewId: number, content: any): Promise<FlexlistsError | FlexlistsSuccess<CloneContentOutputDto>> {
-  var response = await axios.post<FlexlistsError | FlexlistsSuccess<CloneContentOutputDto>>(`/api/listContent/cloneContent`, { viewId, content })
+}
+export async function cloneContent(
+  viewId: number,
+  content: any
+): Promise<FlexlistsError | FlexlistsSuccess<CloneContentOutputDto>> {
+  var response = await axios.post<
+    FlexlistsError | FlexlistsSuccess<CloneContentOutputDto>
+  >(`/api/listContent/cloneContent`, { viewId, content });
 
   return response.data;
-};
-export async function updateContent(viewId: number, content: any): Promise<FlexlistsError | FlexlistsSuccess> {
-  var response = await axios.post<FlexlistsError | FlexlistsSuccess>(`/api/listContent/updateContent`, { viewId, content })
+}
+export async function updateContent(
+  viewId: number,
+  content: any
+): Promise<FlexlistsError | FlexlistsSuccess> {
+  var response = await axios.post<FlexlistsError | FlexlistsSuccess>(
+    `/api/listContent/updateContent`,
+    { viewId, content }
+  );
 
   return response.data;
-};
-export async function updateContents(viewId: number, contents: any[], parentViewId?: number, parentFieldId?: number, parentContentId?: number): Promise<FlexlistsError | FlexlistsSuccess> {
-  var response = await axios.post<FlexlistsError | FlexlistsSuccess>(`/api/listContent/updateContents`, { viewId, contents, parentViewId, parentFieldId, parentContentId })
+}
+export async function updateContents(
+  viewId: number,
+  contents: any[],
+  parentViewId?: number,
+  parentFieldId?: number,
+  parentContentId?: number
+): Promise<FlexlistsError | FlexlistsSuccess> {
+  var response = await axios.post<FlexlistsError | FlexlistsSuccess>(
+    `/api/listContent/updateContents`,
+    { viewId, contents, parentViewId, parentFieldId, parentContentId }
+  );
 
   return response.data;
-};
-export async function deleteBulkContents(viewId: number, contentIds: number[]): Promise<FlexlistsError | FlexlistsSuccess> {
-  var response = await axios.post<FlexlistsError | FlexlistsSuccess>(`/api/listContent/deleteBulkContents`, { viewId, contentIds })
+}
+export async function deleteBulkContents(
+  viewId: number,
+  contentIds: number[]
+): Promise<FlexlistsError | FlexlistsSuccess> {
+  var response = await axios.post<FlexlistsError | FlexlistsSuccess>(
+    `/api/listContent/deleteBulkContents`,
+    { viewId, contentIds }
+  );
 
   return response.data;
-};
-export async function deleteContent(viewId: number, contentId: number): Promise<FlexlistsError | FlexlistsSuccess> {
-  var response = await axios.delete<FlexlistsError | FlexlistsSuccess>(`/api/listContent/deleteContent` + `?viewId=${viewId}&contentId=${contentId}`);
+}
+export async function deleteContent(
+  viewId: number,
+  contentId: number
+): Promise<FlexlistsError | FlexlistsSuccess> {
+  var response = await axios.delete<FlexlistsError | FlexlistsSuccess>(
+    `/api/listContent/deleteContent` +
+      `?viewId=${viewId}&contentId=${contentId}`
+  );
 
   return response.data;
-};
-export async function archiveBulkContents(viewId: number, contentIds: number[]): Promise<FlexlistsError | FlexlistsSuccess> {
-  var response = await axios.post<FlexlistsError | FlexlistsSuccess>(`/api/listContent/archiveBulkContents`, { viewId, contentIds })
+}
+export async function archiveBulkContents(
+  viewId: number,
+  contentIds: number[]
+): Promise<FlexlistsError | FlexlistsSuccess> {
+  var response = await axios.post<FlexlistsError | FlexlistsSuccess>(
+    `/api/listContent/archiveBulkContents`,
+    { viewId, contentIds }
+  );
 
   return response.data;
-};
-export async function unarchiveBulkContents(viewId: number, contentIds: number[]): Promise<FlexlistsError | FlexlistsSuccess> {
-  var response = await axios.post<FlexlistsError | FlexlistsSuccess>(`/api/listContent/unarchiveBulkContents`, { viewId, contentIds })
+}
+export async function unarchiveBulkContents(
+  viewId: number,
+  contentIds: number[]
+): Promise<FlexlistsError | FlexlistsSuccess> {
+  var response = await axios.post<FlexlistsError | FlexlistsSuccess>(
+    `/api/listContent/unarchiveBulkContents`,
+    { viewId, contentIds }
+  );
 
   return response.data;
-};
-export async function archiveContent(viewId: number, contentId: number): Promise<FlexlistsError | FlexlistsSuccess> {
-  var response = await axios.post<FlexlistsError | FlexlistsSuccess>(`/api/listContent/archiveContent`, { viewId, contentId })
+}
+export async function archiveContent(
+  viewId: number,
+  contentId: number
+): Promise<FlexlistsError | FlexlistsSuccess> {
+  var response = await axios.post<FlexlistsError | FlexlistsSuccess>(
+    `/api/listContent/archiveContent`,
+    { viewId, contentId }
+  );
 
   return response.data;
-};
-export async function unarchiveContent(viewId: number, contentId: number): Promise<FlexlistsError | FlexlistsSuccess> {
-  var response = await axios.post<FlexlistsError | FlexlistsSuccess>(`/api/listContent/unarchiveContent`, { viewId, contentId })
+}
+export async function unarchiveContent(
+  viewId: number,
+  contentId: number
+): Promise<FlexlistsError | FlexlistsSuccess> {
+  var response = await axios.post<FlexlistsError | FlexlistsSuccess>(
+    `/api/listContent/unarchiveContent`,
+    { viewId, contentId }
+  );
 
   return response.data;
-};
-export async function getContent(viewId:number,contentId:number): Promise<FlexlistsError|FlexlistsSuccess> {
-  var response = await axios.get<FlexlistsError|FlexlistsSuccess>('/api/listContent/getContent'+`?viewId=${viewId}&contentId=${contentId}`)
+}
+export async function getContent(
+  viewId: number,
+  contentId: number
+): Promise<FlexlistsError | FlexlistsSuccess> {
+  var response = await axios.get<FlexlistsError | FlexlistsSuccess>(
+    "/api/listContent/getContent" + `?viewId=${viewId}&contentId=${contentId}`
+  );
   return response.data;
-};
-export async function searchContents(viewId: number, page?: number, limit?: number, order?: Sort[], query?: any, conditions?: any, includeCount?: boolean, parentViewId?: number, parentFieldId?: number, parentContentId?: number): Promise<FlexlistsError | FlexlistsSuccess<SearchContentsOutputDto>> {
-  var response = await axios.post<FlexlistsError | FlexlistsSuccess<SearchContentsOutputDto>>(`/api/listContent/searchContents`, { viewId, page, limit, order, query, conditions, includeCount, parentViewId, parentFieldId, parentContentId })
+}
+export async function searchContents(
+  viewId: number,
+  page?: number,
+  limit?: number,
+  order?: Sort[],
+  query?: any,
+  conditions?: any,
+  includeCount?: boolean,
+  parentViewId?: number,
+  parentFieldId?: number,
+  parentContentId?: number
+): Promise<FlexlistsError | FlexlistsSuccess<SearchContentsOutputDto>> {
+  var response = await axios.post<
+    FlexlistsError | FlexlistsSuccess<SearchContentsOutputDto>
+  >(`/api/listContent/searchContents`, {
+    viewId,
+    page,
+    limit,
+    order,
+    query,
+    conditions,
+    includeCount,
+    parentViewId,
+    parentFieldId,
+    parentContentId,
+  });
 
   return response.data;
-};
-export async function search(type: SearchType, viewId?: number, page?: number, limit?: number, order?: Sort[], query?: Query, conditions?: any): Promise<FlexlistsError | FlexlistsSuccess<SearchOutputDto>> {
-  var response = await axios.post<FlexlistsError | FlexlistsSuccess<SearchOutputDto>>(`/api/listContent/search`, { type, viewId, page, limit, order, query, conditions })
+}
+export async function search(
+  type: SearchType,
+  viewId?: number,
+  page?: number,
+  limit?: number,
+  order?: Sort[],
+  query?: Query,
+  conditions?: any
+): Promise<FlexlistsError | FlexlistsSuccess<SearchOutputDto>> {
+  var response = await axios.post<
+    FlexlistsError | FlexlistsSuccess<SearchOutputDto>
+  >(`/api/listContent/search`, {
+    type,
+    viewId,
+    page,
+    limit,
+    order,
+    query,
+    conditions,
+  });
 
   return response.data;
-};
+}
